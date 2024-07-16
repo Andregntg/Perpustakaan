d6df4fec5abc2c81ab1d198053ffea0870216cbf
Andregntg <gifanoandre6@gmail.com>
2024-07-17 01:59:50 +0700

    update

--- resources/views/User/dashboard.blade.php
+++ resources/views/User/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container mx-auto mt-5">
-        <h1 class="text-3xl font-bold mb-6 text-gray-800">User Dashboard</h1>
+        <h1 class="text-3xl font-bold mb-6 text-gray-800">User Dashboard di sini</h1>
 
         <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
             <!-- Buku Card -->
